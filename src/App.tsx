3e78cbe14cb0e54ed6dcae653f7a57a6a64a1e4d
Hossein Salari <hossein.salari002@gmail.com>
2023-10-10 16:25:23 +0330

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <>
-    </>
+    <div className="bg-[#F6F7F9;] min-h-screen">
+      <Navbar />
+    </div>
   );
 }
 
