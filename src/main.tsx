0309809249e4e30d86a16d67c786455c7dae7573
Hossein Salari <hossein.salari002@gmail.com>
2023-12-27 21:29:29 +0330

    refactor: Refactoring service worker files to ts

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
+import * as serviceWorkerRegistration from "../serviceWorkerRegistration.ts";
+
 import "react-loading-skeleton/dist/skeleton.css";
 import "yet-another-react-lightbox/styles.css";
 import "react-widgets/styles.css";
@@ -30,3 +32,8 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </AuthContextProvider>
   </RentCarContextProvider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
