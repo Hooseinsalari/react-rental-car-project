ec56d20d57efa4edc7005cbe35e0d9cd8e185388
Hossein Salari <hossein.salari002@gmail.com>
2023-12-28 20:43:07 +0330

    fix: change all template for pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-import * as serviceWorkerRegistration from "../serviceWorkerRegistration.ts";
-
 import "react-loading-skeleton/dist/skeleton.css";
 import "yet-another-react-lightbox/styles.css";
 import "react-widgets/styles.css";
@@ -32,8 +30,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </AuthContextProvider>
   </RentCarContextProvider>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,63 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "maskable.png",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+        "logo144.png",
+        "logo192.png",
+        "logo256.png",
+        "logo384.png",
+        "logo512.png",
+      ],
+      manifest: {
+        name: "Morent",
+        short_name: "Morent",
+        theme_color: "#2196f3",
+        icons: [
+          {
+            src: "maskable.png",
+            sizes: "196x196",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "logo144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "logo384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
